ff9812ff5621f6856905a03b79dfec2feee96007
chirag s <138520615+0-chirag-s@users.noreply.github.com>
2024-12-17 20:32:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Hero, Features, Pricing, Testimonials, Footer, FAQ } from './sections';
+import { Hero, Features, Pricing, Testimonials, Footer, FAQ } from './sections/index';
 import NavBar from './components/NavBar';
 //aiadj
 
